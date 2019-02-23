4c129c0fadc392a1bc282254e16d0a9184590b57
Luke Vo <datvm@outlook.com>
2019-02-23 11:25:05 +0700

    Added Email Service and Test.

--- WebMonitor.ServiceCore/Services/EmailService.cs
+++ WebMonitor.ServiceCore/Services/EmailService.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +18,76 @@ namespace WebMonitor.ServiceCore.Services
 
     public class EmailService : IEmailService
     {
-        public Task SendEmailAsync(string from, string[] to, string subject, string html)
+        
+        public async Task SendEmailAsync(string from, string[] to, string subject, string html)
         {
-            throw new NotImplementedException();
+            var request = WebRequest.CreateHttp("https://api.mailjet.com/v3.1/send");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            var username = "2414c889fc40419c209edfe5c158e335";
+            var password = "bd6e704c1b9ee8c975c1b821bd104db4";
+            var authKey = Convert.ToBase64String(
+                Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
+            request.Headers.Add("Authorization", "Basic " + authKey);
+
+            using (var requestBody = await request.GetRequestStreamAsync())
+            {
+                using (var streamWriter = new StreamWriter(requestBody))
+                {
+                    await streamWriter.WriteAsync(JsonConvert.SerializeObject(new
+                    {
+                        Messages = new object[] {
+                            new {
+                                From = new
+                                {
+                                    Email = from,
+                                    Name = from,
+                                },
+                                To = to.Select(q => new
+                                {
+                                    Email = q,
+                                    Name = q,
+                                }).ToList(),
+                                Subject = subject,
+                                HtmlPart = html,
+                            }
+                        },
+                    }));
+                }
+            }
+
+            try
+            {
+                using (var response = await request.GetResponseAsync())
+                {
+#if DEBUG
+                    using (var responseBody = response.GetResponseStream())
+                    {
+                        using (var streamReader = new StreamReader(responseBody))
+                        {
+                            var responseText = await streamReader.ReadToEndAsync();
+                            Debugger.Break();
+                        }
+                    }
+#endif
+                }
+            }
+            catch (WebException ex)
+            {
+#if DEBUG
+                using (var responseBody = ex.Response.GetResponseStream())
+                {
+                    using (var streamReader = new StreamReader(responseBody))
+                    {
+                        var responseText = await streamReader.ReadToEndAsync();
+                        Debugger.Break();
+                    }
+                }
+#endif
+                throw;
+            }
+
         }
     }
 }
---
+++ WebMonitor.Test/ServiceCore/Services/EmailServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebMonitor.ServiceCore.Services;
+using Xunit;
+
+namespace WebMonitor.Test.ServiceCore.Services
+{
+    public class EmailServiceTest
+    {
+
+        IEmailService emailService;
+
+        public EmailServiceTest()
+        {
+            this.emailService = new EmailService();
+        }
+
+        [Fact]
+        public async Task SendEmailAsyncTest()
+        {
+            await this.emailService.SendEmailAsync(
+                "nazozotole@mail-hub.info",
+                new string[] { "nazozotole@mail-hub.info", },
+                "Testing Email",
+                "Email content <p style='red'>Abc</p>");
+        }
+
+    }
+}
